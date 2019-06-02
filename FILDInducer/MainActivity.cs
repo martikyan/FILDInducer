2d3ba8826f78c7db63ad316bdc42aac666f54e11
Karen Martikyan <karen@concent.io>
2019-06-02 18:00:33 +0400

    Initial not working state

--- FILDInducer/MainActivity.cs
+++ FILDInducer/MainActivity.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
+using Android.Content;
 using Android.OS;
-using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using System;
+using System.Threading.Tasks;
 
 namespace FILDInducer
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private ProgressBar _progressBar;
+        private TimeoutManager _timeoutManager = new TimeoutManager(TimeSpan.FromSeconds(5));
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -21,8 +24,24 @@ namespace FILDInducer
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            _progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
+            _timeoutManager.TimeoutReached += _timeoutManager_TimeoutReached;
+
+            var layout = FindViewById<CoordinatorLayout>(Resource.Id.coordinatorLayout);
+            layout.Touch += Layout_Touch;
+
+            Task.Run(ProgressBarUpdater);
+        }
+
+        private void Layout_Touch(object sender, View.TouchEventArgs e)
+        {
+            _timeoutManager.Restart();
+        }
+
+        private void _timeoutManager_TimeoutReached(object sender, EventArgs e)
+        {
+            var toast = Toast.MakeText(Application.Context, "Hello", ToastLength.Long);
+            toast.Show();
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -42,12 +61,14 @@ namespace FILDInducer
             return base.OnOptionsItemSelected(item);
         }
 
-        private void FabOnClick(object sender, EventArgs eventArgs)
+        private async Task ProgressBarUpdater()
         {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+            while (_timeoutManager.Running)
+            {
+                _progressBar.Progress = (int) _timeoutManager.ProgressPercentage;
+                await Task.Delay(10);
+            }
         }
-	}
+    }
 }
 
---
+++ FILDInducer/TimeoutManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FILDInducer
+{
+    internal sealed class TimeoutManager
+    {
+        public TimeSpan Timeout { get; }
+        public bool Running { get; private set; } = true;
+        public float ProgressPercentage => (float) (100.0 * _stopwatch.ElapsedMilliseconds / Timeout.TotalMilliseconds);
+        public event EventHandler TimeoutReached;
+
+        private readonly Timer _timer;
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        public TimeoutManager(TimeSpan timeout)
+        {
+            if (timeout == TimeSpan.Zero)
+            {
+                throw new ArgumentNullException(nameof(timeout));
+            }
+
+            Timeout = timeout;
+            _timer = new Timer(Timeout.TotalMilliseconds)
+            {
+                AutoReset = false,
+            };
+            _stopwatch.Start();
+            _timer.Elapsed += TimeoutReachedHandler;
+        }
+
+        public void Restart()
+        {
+            _timer.Stop();
+            _timer.Start();
+            _stopwatch.Restart();
+        }
+
+        private void TimeoutReachedHandler(object sender, ElapsedEventArgs e)
+        {
+            Running = false;
+            _stopwatch.Stop();
+            _timer.Stop();
+            TimeoutReached?.Invoke(sender, e);
+        }
+    }
+}
